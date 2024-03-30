2e7bf8e11ac8fedf85cde8ef9e19db5bc6ccac57
ChonkMOnk <richardh110604@gmail.com>
2024-03-30 16:44:11 -0400

    Cleaned Nulls

--- cleaning.py
+++ cleaning.py
@@ -4,6 +4,6 @@ import numpy as np
 import gzip
 
 with gzip.open('variables.pkl.gz', 'rb') as f:
-    X_train, X_test, y_train, y_test = pickle.load(f)
+    X_train, X_test, X_train_norm, X_test_norm, y_train, y_test = pickle.load(f)
 
-print(X_train[:20])
+print(X_train.head())
