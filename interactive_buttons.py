717e5787933722ab099f784069ca5882399dd2ae
ChonkMOnk <richardh110604@gmail.com>
2024-03-30 23:13:55 -0400

    Front End!

--- interactive_buttons.py
+++ interactive_buttons.py
@@ -1,118 +1,87 @@
-"""
-from reactpy import component, html, run
-
-@component
-def IdentifyGender():
-    def handle_gender_choice(event):
-        gender = event.get('target', {}).get('value')
-        print("Selected gender:", gender)
-    
-    return html.form(
-        html.label({"for": "male"}, "Male"),
-        html.input({"type": "radio", "name": "gender", "id": "male", "value": "male", "on_change": handle_gender_choice}),
-        html.label({"for": "female"}, "Female"),
-        html.input({"type": "radio", "name": "gender", "id": "female", "value": "female", "on_change": handle_gender_choice}),
-        html.label({"for": "other"}, "Other"),
-        html.input({"type": "radio", "name": "gender", "id": "other", "value": "other", "on_change": handle_gender_choice})
-    )
-
-
-@component
-def IdentifyRace():
-    def handle_race_choice(event):
-        race = event.get('target', {}).get('value')
-        print("Selected race:", race)
-    
-    return html.form(
-        html.label({"for": "native"}, "American Indian or Alaska Native"),
-        html.input({"type": "radio", "name": "race", "id": "native", "value": "native", "on_change": handle_race_choice}),
-        html.label({"for": "asian"}, "Asian"),
-        html.input({"type": "radio", "name": "race", "id": "asian", "value": "asian", "on_change": handle_race_choice}),
-        html.label({"for": "black"}, "Black or African American"),
-        html.input({"type": "radio", "name": "race", "id": "black", "value": "black", "on_change": handle_race_choice}),
-        html.label({"for": "hawaiian"}, "Native Hawaiian or Other Pacific Islander"),
-        html.input({"type": "radio", "name": "race", "id": "hawaiian", "value": "hawaiian", "on_change": handle_race_choice}),
-        html.label({"for": "white"}, "White"),
-        html.input({"type": "radio", "name": "race", "id": "white", "value": "white", "on_change": handle_race_choice})
-    )
-
-
-
-American Indian or Alaska Native (DONE)
-Asian (DONE)
-Black or African American (DONE)
-Native Hawaiian or Other Pacific Islander (DONE)
-White (DONE)
-*Co-applicant race 
-*Information not provided by applicant in mail, Internet, or telephone application
-*No co-applicant
-*Not applicable
-
-
-@component
-def IdentityForm():
-    return html.div(
-        IdentifyGender(),
-        IdentifyRace()
-    )
-
-run(IdentityForm)
-"""
 import pandas as pd
 import numpy as np
 import csv
 
-list_variables = []
-list_headings = ['Gender']
-
 def save_results():
     # Retrieve the selected values from the variables
-    selected_gender = gender_var.get()
-    list_variables.append(selected_gender)
+    global list_headings
+    global inp_gender
+    global inp_property_type
+    global inp_loan_amount
+    global inp_loan_type
+    global inp_loan_purp
+    global inp_lien_status
+    global inp_owner_occ
+    # global inp_income 
+    list_headings = ['Gender', 'Property Type', 'Loan Amount', 'Loan Type', 
+                     'Loan Purpose', 'Lien Status', 'Owner Occupancy']
 
+    # no median family income for census
+
+    # inp_gender = gender_var.get()
+    inp_gender = gender_var.get()
+    inp_property_type = property_type.get()
+    inp_loan_amount = loan_amount.get()
+    inp_loan_amount = int(inp_loan_amount)
+    inp_loan_type = loan_type.get()
+    inp_loan_purp = loan_purp.get()
+    inp_lien_status = lien_status.get()
+    inp_owner_occ = owner_occ.get()
+    # inp_income = income.get()
+    list_variables = [inp_gender, inp_property_type, inp_loan_amount, inp_loan_type, inp_loan_purp, inp_lien_status, inp_owner_occ]
+    heading_variable_pairs = {}
+    for i in range(len(list_headings) - 1):
+        heading_variable_pairs[list_headings[i]] = list_variables[i]
 
 import tkinter as tk
 
 root = tk.Tk()
 root.title("INSERT PROGRAM TITLE HERE")
+root.geometry('900x500')
+main_frame = tk.LabelFrame(
+    root,
+    bg = "#F3D5B9",
+    text = 'Inputs',
+)
+main_frame.place(anchor = 'c', relx = 0.5, rely=0.21)
 
 # Initalizing all parameter variables
 gender_var = tk.StringVar()
+loan_amount = tk.StringVar()
+property_type = tk.StringVar()
+loan_type = tk.StringVar()
+loan_purp = tk.StringVar()
+lien_status = tk.StringVar()
+owner_occ = tk.StringVar()
+# income = tk.IntVar()
 
-# BUTTONS FOR GENDER
-label = tk.Label(root, text="What is your gender?")
-label.pack(padx=25, pady=15)
+# LABLES
+gender_label = tk.Label(main_frame, text="What is your gender?").grid(row = 1, column = 0)
+property_type_label = tk.Label(main_frame, text="What is your property type?").grid(row = 2, column = 0)
+loan_amount_label = tk.Label(main_frame, text = 'How Much are you applying for?').grid(row = 3, column = 0)
+loan_type_label = tk.Label(main_frame, text="What Type of Loan are you Applying for?").grid(row = 4, column = 0)
+loan_purp_label = tk.Label(main_frame, text="Why are you applying for the Loan?").grid(row = 5, column = 0)
+lien_status_label = tk.Label(main_frame, text="What is your Lien Status?").grid(row = 6, column = 0)
+owner_occ_label = tk.Label(main_frame, text="Are you planning on Residing in the Residence?").grid(row = 7, column = 0)
 
-# Create three frames for the columns
-left_frame = tk.Frame(root)
-middle_frame = tk.Frame(root)
-right_frame = tk.Frame(root)
+# OPTIONS
+gender_options = ['Male', 'Female', 'Other']
+property_type_options = ['One-to-four family dwelling (other than manufactured housing)', 'Manufactured housing', 'Multifamily dwelling']
+loan_type_options = ['Conventional','FHA-insured','VA-guaranteed','FSA/RHS-guaranteed']
+loan_purpose_options = ['Refinancing','Home purchase','Home improvement']
+lien_status_options = ['Secured by a first lien','Not Applicable','Secured by a subordinate lien','Not Secured by a lien']
+owner_occ_options = ['Owner-occupied as a principal dwelling','Not Owner-occupied as a principal dwelling','Not Applicable']
 
-# Pack the frames side by side
-left_frame.pack(side="left", fill="both", expand=True)
-middle_frame.pack(side="left", fill="both", expand=True)
-right_frame.pack(side="right", fill="both", expand=True)
-
-# Create radio buttons in each frame
-radio_button1 = tk.Radiobutton(right_frame, text="Male", variable=gender_var, value="Male")
-radio_button2 = tk.Radiobutton(left_frame, text="Female", variable=gender_var, value="Female")
-radio_button3 = tk.Radiobutton(middle_frame, text="Other", variable=gender_var, value="Other")
-
-# Pack the radio buttons within each frame
-radio_button1.pack(side="top", anchor="w")
-radio_button2.pack(side="top", anchor="w")
-radio_button3.pack(side="top", anchor="w")
+gender_menu = tk.OptionMenu(main_frame, gender_var, *gender_options).grid(row = 1, column = 1)
+property_type_menu = tk.OptionMenu(main_frame, property_type, *property_type_options).grid(row = 2, column = 1)
+loan_amount_menu = tk.Entry(main_frame, textvariable=loan_amount).grid(row = 3, column = 1)
+loan_type_menu = tk.OptionMenu(main_frame, loan_type, *loan_type_options).grid(row = 4, column = 1)
+loan_purp_menu = tk.OptionMenu(main_frame, loan_purp, *loan_purpose_options).grid(row = 5, column = 1)
+lien_status_menu = tk.OptionMenu(main_frame, lien_status,*lien_status_options).grid(row = 6, column = 1)
+owner_occ_menu = tk.OptionMenu(main_frame, owner_occ,*owner_occ_options).grid(row = 7, column = 1)
 
 # Saving all current chosen variables when the user presses save
 save_button = tk.Button(root, text="Save Results", command=save_results)
 save_button.pack(pady=10)
-
-heading_variable_pairs = {}
-for i in range(len(list_headings) - 1):
-    heading_variable_pairs[list_headings[i]] = list_variables[i]
-
-with open('test.csv', 'w') as f:
-    for key in heading_variable_pairs.keys():
-        f.write("%s,%s\n"%(key,heading_variable_pairs[key]))
 
 root.mainloop()
