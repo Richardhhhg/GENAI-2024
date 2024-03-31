2a0dbff651645cc7c9a0246a616c4bc61117c821
Richard Hong <81711255+Richardhhhg@users.noreply.github.com>
2024-03-30 23:21:12 -0400

    Delete cleaning.py
    GARBAGE

--- cleaning.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import pandas as pd
-import numpy as np
-import gzip
-
-with gzip.open('variables.pkl.gz', 'rb') as f:
-    X_train, X_test, X_train_norm, X_test_norm, y_train, y_test = pickle.load(f)
-
-print(X_train.head())
